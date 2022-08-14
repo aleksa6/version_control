9826436cf0dc2dec9b08c6642469e37ca0949f86
OgiBlade <ognjenstefanovic.2005@gmail.com>
2022-08-14 19:42:02 +0200

    Funkcija add i status koji radi

--- library.py
+++ library.py
@@ -3,43 +3,64 @@ import filecmp
 from pickle import FALSE
 cwd = os.getcwd()
 files = []
-added_files = ['test.txt']
+added_files = []
 main_dir = cwd
 dir = os.path.join(cwd, 'vc')
 
 def check():
-    if not os.path.exists(dir):
+    if not  os.path.exists(dir):
+        print("vc directory doesn't exist")
         return 0
     return 1
 
+def add_to_files():
+    for file in os.listdir(main_dir):
+         if os.path.isfile(os.path.join(main_dir, file)):
+            if file not in files and file not in added_files:
+                files.append(file)
+
+    files.remove('GIT.docx')
+    files.remove('library.py')
+    
+
 def init():
-    if check():
+    if os.path.exists(dir):
         print("vc directory already exists")
         return 1
     os.mkdir(dir)
     print("vc directory made")
 
 def status():
-    if not check():
-        print("vc directory doesn't exist")
-        return 0
+    check()
+
+    add_to_files()
     
-
-
-    for file in os.listdir(main_dir):
-     if os.path.isfile(os.path.join(main_dir, file)):
-            files.append(file)
-    files.remove('GIT.docx')
-    files.remove('library.py')
-    for file in added_files:
-        files.remove(file)
     match, mismatch, errors = filecmp.cmpfiles(dir, main_dir, files, shallow= False)
-    print('\033[1;32;40m', added_files)
+    added_files_for_print = list(set(added_files) - set(match))
+    print('\033[1;32;40m', added_files_for_print )
     mismatch += errors
     print('\033[1;31;40m',mismatch)
     print('\033[0;37;40m')
 
 
+def add(file):
+    check()
+    add_to_files()
+    
+    if file in files and file not in added_files:
+        added_files.append(file)
+        files.remove(file)
+        print('File ', file, ' staged for commit')
+        return 0
+    
+    elif file in added_files:
+        print('File ', file, ' already staged for commit')
+        return 0
+    
+    print("File doesn't exist" )
+
+f = 'test.txt'
+add(f)
 status()
 
     
