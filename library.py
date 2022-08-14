d249b790d626c41ad1846fbb87d7c82590d62db5
OgiBlade <ognjenstefanovic.2005@gmail.com>
2022-08-14 18:42:39 +0200

    Test za status

---
+++ library.py
@@ -0,0 +1,40 @@
+import os
+import filecmp
+from pickle import FALSE
+cwd = os.getcwd()
+files = []
+main_dir = cwd
+dir = os.path.join(cwd, 'vc')
+
+def check():
+    if not os.path.exists(dir):
+        return 0
+    return 1
+
+def init():
+    if check():
+        print("vc directory already exists")
+        return 1
+    os.mkdir(dir)
+    print("vc directory made")
+def status():
+
+    for file in os.listdir(main_dir):
+     if os.path.isfile(os.path.join(main_dir, file)):
+            files.append(file)
+    files.remove('GIT.docx')
+    files.remove('library.py')
+    match, mismatch, errors = filecmp.cmpfiles(dir, main_dir, files, shallow= False)
+    print('\033[1;31;40m',mismatch, errors)
+    print('\033[0;37;40m')
+
+
+status()
+
+    
+
+
+
+
+
+
