b54397a14eae335b029dffdea6312c29ad1a1804
OgiBlade <ognjenstefanovic.2005@gmail.com>
2022-08-16 15:56:40 +0200

    log koji bi trebao da radi

--- log.py
+++ log.py
@@ -2,26 +2,6 @@ import os
 import json
 import sys
 
-<<<<<<< Updated upstream
-from util import isInitialized, load_data, update_data, get_files, get_new_files, validate_args, format_file_data
-from variables import bcolors, main_dir, vcs_path, paths_to_ignore  
-
-commits_path = os.path.join(vcs_path, "branches", data["info"]["current_branch"])
-files_in_branch = os.listdir(commits_path)
-files_in_branch = os.path.join(files_in_branch, 'commits')
-
-def log():
-    for file in files_in_branch:
-        print(bcolors.WARNING + file+ bcolors.ENDC ) 
-
-
-def checkout(name_of_branch):
-    data = load_data()
-    if not name_of_branch in data["info"]['branches']:
-        return 0
-    data["info"]["current_branch"] = name_of_branch
-
-=======
 from util import  load_data, update_data, get_files, get_new_files, validate_args, format_file_data
 from variables import bcolors, main_dir, vcs_path, paths_to_ignore  
 data = load_data()
@@ -41,6 +21,6 @@ def checkout(name_of_branch):
         return 0
     data["info"]["current_branch"] = name_of_branch
     update_data(data)
->>>>>>> Stashed changes
+
 
 
--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 import os
 import sys
 import shutil
-from turtle import update
+import filecmp
 
-from util import is_initialized, load_data, update_data, get_files, get_new_files, validate_args, format_file_data
+from util import is_initialized, load_data, update_data, get_files, validate_args, format_file_data
 from variables import bcolors, main_dir, vcs_path, paths_to_ignore  
-<<<<<<< Updated upstream
-  
-=======
 from commit import commit
 from log import flog, checkout
->>>>>>> Stashed changes
 def status():
-  pass
-      
+  data = load_data()
+  branch = data["info"]["current_branch"]
+  
+  commits = data["info"]["branches"][branch]["commits"]
+
+  if len(commits) < 1:
+    for file in data["saved_files"]:
+      print(bcolors.OKGREEN + "new file:         " + file["name"] + bcolors.ENDC)
+    for file in get_files(main_dir):
+      if file["path"] not in [file["path"] for file in data["saved_files"]]:
+        print(bcolors.FAIL + "untracked:        " + file["name"] + bcolors.ENDC)
+    return
+  
+  commons = [file["name"] for file in commits[-1]["files"]]
+  
+  for file in get_files(main_dir):
+    if file["name"] not in commons:
+      if file["path"] in [data["path"] for data in data["saved_files"]]:
+        print(bcolors.OKGREEN + "new file:         " + file["name"] + bcolors.ENDC)
+      else:
+        print(bcolors.FAIL + "untracked:        " + file["name"] + bcolors.ENDC)
+  
+  match, missmatch, errors = filecmp.cmpfiles(main_dir, commits[-1]["path"], commons)
+
+  for filename in match:
+    print(bcolors.OKGREEN + "up to date:       " + file["name"] + bcolors.ENDC)
+  for filename in missmatch:
+    print(bcolors.FAIL + "modified:         " + filename + bcolors.ENDC)
+  for filename in errors:
+    print(bcolors.FAIL + "deleted:          " + filename + bcolors.ENDC)
+
 def add_files(files):
   data = load_data()
   file_paths = [file["path"] for file in data["saved_files"]]
   for file in files:
-    if file["path"] not in file_paths and file["path"] not in paths_to_ignore:
+    if file["path"] not in file_paths and file["path"] not in paths_to_ignore and os.path.exists(file["path"]) and os.path.isfile(file["path"]):
       data["saved_files"].append(file)
       update_data(data)
+    else:
+      print("Could not add " + file["name"] + " to staging area")
 
 
 def add_all_files():
@@ -28,16 +55,25 @@ def add_all_files():
   data["saved_files"] = get_files(main_dir)
   data["saved_files"] = list(map(lambda file : { "path": file["path"], "relative_path": file["relative_path"], "name": file["name"] }, data["saved_files"]))
   update_data(data)
-  
-def help():
-  pass
 
-def branch(name):
-  # os.makedirs(os.path.join(main_dir, "vcs", "branches", name))
-  branches = os.listdir(os.path.join(main_dir, "vcs", "branches"))
-  if name not in branches:
-    data = load_data()
-    data["info"]["branches"].append(name)
+get_branches = lambda : os.listdir(os.path.join(main_dir, "vcs", "branches"))
+
+def log_branches():
+  data = load_data()
+  for branch in get_branches():
+    if branch == data["info"]["current_branch"]:
+      print(bcolors.OKGREEN + branch + bcolors.ENDC)
+    else:
+      print(branch)
+
+def create_branch(names):
+  data = load_data()
+  branches = get_branches()
+  for name in names:
+    if name.isalpha() and name not in branches:
+      os.makedirs(os.path.join(main_dir, "vcs", "branches", name))
+      data["info"]["branches"].append(name)
+  update_data(data)
     
 def init():
   reinit = False
@@ -52,7 +88,7 @@ def init():
   open(os.path.join(vcs_path, "data", "data.json"), "x")
   
   data = load_data()
-  data = { "saved_files": [], "info": { "branches": ["main"] } }
+  data = { "saved_files": [], "info": { "branches": { "main": {"commits": [] } }, "current_branch": "main"  } }
   update_data(data)
   
   if reinit: print("Project reinitialized") 
@@ -75,9 +111,6 @@ def main():
       add_files(args)
   elif cmd == "branch":
     if len(args) < 3:
-<<<<<<< Updated upstream
-      branch("test")
-=======
       log_branches()
     else:
       create_branch(args[2:])
@@ -85,8 +118,5 @@ def main():
     if not args[2]: return print("You have to specify name of the commit")
     commit(args[2])
   elif cmd == "log": flog()
->>>>>>> Stashed changes
 
 main()
-
-# filter(lambda x )
